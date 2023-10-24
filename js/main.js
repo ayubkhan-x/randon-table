78e9b50ef31544603ea7ae0e83a6a4c2837998b6
ayubkhan-x <khakimovayubkhan@gmail.com>
2023-10-24 07:43:13 -0700

    init

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let table1 = document.getElementById('table1')
+let table2 = document.getElementById('table2')
+let table3 = document.getElementById('table3')
+let table4 = document.getElementById('table4')
+let table5 = document.getElementById('table5')
+let table6 = document.getElementById('table6')
+let table7 = document.getElementById('table7')
+let table8 = document.getElementById('table8')
+let table9 = document.getElementById('table9')
+let table10 = document.getElementById('table10')
+
+let arr = [table1, table2, table3, table4, table5, table6, table7, table8, table9, table10]
+
+
+
+
+function tableSelector() {
+    
+    // console.log(arr[(Math.floor(Math.random() * arr.length))]);
+}
+tableSelector()
